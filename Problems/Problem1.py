7e78baaabea26ad73452661ede81fedce5a062cb
Martin Poulsen <Martin_Poulsen1@live.dk>
2020-10-17 19:37:42 +0200

    Added shortcode for Problem1.py. Added placeholder for Problem2.py

--- Problems/Problem1.py
+++ Problems/Problem1.py
@@ -7,3 +7,7 @@ for i in range(c):
     if i % 3 == 0 or i % 5 == 0:
         sumOfMultiples += i
 print(sumOfMultiples)
+
+
+def shortcode():
+    print(sum(n for n in range(1000) if n % 3 == 0 or n % 5 == 0))
