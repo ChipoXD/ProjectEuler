238d09c0246d8aef5a0188b935c92c4d9e38c29c
Martin Poulsen <Martin_Poulsen1@live.dk>
2020-10-18 01:33:07 +0200

    Updated Problem6.py with better math.

--- Problems/Problem6.py
+++ Problems/Problem6.py
@@ -1,7 +1,7 @@
 # ProblemURL: https://projecteuler.net/problem=6
 a = 100
 sumOfSquare = (a * (a + 1) * (2 * a + 1)) / 6  # Sum of the Squares formula
-squareOfSum = ((a / 2) * (1 + a))**2  # Gauss sum formula**2
+squareOfSum = (a * (a + 1) / 2)**2  # Gauss sum formula**2
 print(squareOfSum-sumOfSquare)
 # https://www.nctm.org/Publications/Teaching-Children-Mathematics/Blog/The-Story-of-Gauss/
 # https://www.math-only-math.com/sum-of-the-squares-of-first-n-natural-numbers.html
