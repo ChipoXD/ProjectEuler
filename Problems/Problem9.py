eb08f8f85b6e2ee25f31459196f2cb7a26d6c1ff
Martin Poulsen <Martin_Poulsen1@live.dk>
2020-10-18 02:47:20 +0200

    Updated Problem9.py with better description.

--- Problems/Problem9.py
+++ Problems/Problem9.py
@@ -2,9 +2,9 @@
 # We can generate Pythagorean triplet a^2 + b^2 = c^2
 # a = m**2 - n**2,  b = 2 * n * m,  c = n**2 + m**2 for m > n
 # ---------------------------------------------------------------
-# We need to find a + b + c = 100, which means:
-# m^2 - n^2 + 2nm + n^2 + m^2 = 100
-# n = (50/m) - m for m > n
+# We need to find a + b + c = 1000, which means:
+# m^2 - n^2 + 2nm + n^2 + m^2 = 1000
+# n = (500/m) - m for m > n
 
 m = 2
 while True:
