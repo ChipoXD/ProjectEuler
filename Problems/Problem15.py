617dae93817a367474508ff7b91021947c80ea3d
Martin Poulsen <Martin_Poulsen1@live.dk>
2020-10-19 17:37:39 +0200

    Solved Problem15.py

--- Problems/Problem15.py
+++ Problems/Problem15.py
@@ -1,1 +1,14 @@
-# ProblemURL: https://projecteuler.net/problem=14
+# ProblemURL: https://projecteuler.net/problem=15
+def fact(x):
+    f = 1
+    for i in range(1, x + 1):
+        f *= i
+    return f
+
+
+def binomial(argin1, argin2):
+    return (fact(argin1))/(fact(argin2)*(fact(argin1-argin2)))
+
+
+a = 20
+print(binomial(a*2, a))
