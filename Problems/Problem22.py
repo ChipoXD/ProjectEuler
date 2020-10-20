a45203ebd4a18c2d0760d9e50d44c4b473b80769
Martin Poulsen <Martin_Poulsen1@live.dk>
2020-10-20 02:57:42 +0200

    Solved Problem22.py

--- Problems/Problem22.py
+++ Problems/Problem22.py
@@ -1,1 +1,17 @@
-# ProblemURL: https://projecteuler.net/problem=22
+# ProblemURL: https://projecteuler.net/problem=2
+f = open('Problem22data.txt', 'r')
+content = f.read().split("\",\"")
+f.close()
+content[0] = content[0][1:]
+content[-1] = content[-1][:-1]
+content.sort()
+
+totalSum = 0
+for name in content:
+    nameSum = 0
+    for c in name:
+        nameSum += (ord(c) - 64)
+    nameSum *= (content.index(name) + 1)
+    totalSum += nameSum
+
+print(totalSum)
